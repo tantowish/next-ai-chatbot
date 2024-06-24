fac916527971cad7ceae27feec2d9895de1afaa3
tanto <tantows001@gmail.com>
2024-06-25 00:13:41 +0700

    feat: gemini, claude model added

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,30 +1,59 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { StreamTextResult, streamText } from 'ai';
+import { google } from '@ai-sdk/google';
 import { NextResponse } from 'next/server';
-
-export const runtime = 'edge'
-const openai = new OpenAI({
-  apiKey: process.env.api_key || '',
-});
+import { anthropic } from '@ai-sdk/anthropic';
 
 export async function POST(req: Request) {
     try{
-        if(!process.env.api_key){
-            return new NextResponse('Missing Openai API Key.', {status: 400})
-        }    
         const { messages, data } = await req.json();
-        // console.log(data)
+        console.log(messages)
+        
+        const model = data.model
+        console.log(model)
     
-        // Ask OpenAI for a streaming chat completion given the prompt
-        const response = await openai.chat.completions.create({
-            model: 'gpt-4o',
-            stream: true,
-            messages,
-        });
-    
-        const stream = OpenAIStream(response);
-    
-        return new StreamingTextResponse(stream);
+        if(model == 'chatgpt'){
+            if(!process.env.OPENAI_API_KEY){
+                return new NextResponse('Missing Openai API Key.', {status: 400})
+            }    
+
+            console.log('Getting GPT response')
+            const response = await streamText({
+                model: openai('gpt-4'),
+                messages,
+            });
+
+            return response.toAIStreamResponse()
+        }
+        else if(model == 'gemini'){
+            if(!process.env.GOOGLE_GENERATIVE_AI_API_KEY){
+                return new NextResponse('Missing Gemini API Key.', {status: 400})
+            } 
+
+            console.log('Getting Gemini response')
+            const response = await streamText({
+                model : google('models/gemini-1.5-pro-latest'),
+                messages,
+            })
+
+            return response.toAIStreamResponse()
+        }
+        else if(model == 'claude'){
+            if(!process.env.ANTHROPIC_API_KEY){
+                return new NextResponse('Missing Antrhopic (Claude) API Key.', {status: 400})
+            } 
+
+            console.log('Getting Claude response')
+            const response = await streamText({
+                model: anthropic('claude-3-5-sonnet-20240620'),
+                messages,
+            })
+
+            return response.toAIStreamResponse()
+        }
+        else {
+            return new NextResponse('Model required.', {status: 400})
+        }
     } catch(error: any) {
         return new NextResponse(error.message || 'Something went wrong!', {
             status: 500
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,38 +1,64 @@
 'use client'
 
-import { useChat } from "ai/react";
+import { useChat, Message } from "ai/react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { SendHorizonalIcon } from "lucide-react";
 import { ScrollArea } from "./ui/scroll-area";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import CopyToClipboard from "./copyToClipboard";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"  
 import Markdown from "react-markdown";
+import { Textarea } from "./ui/textarea";
 
 
 export const Chat = () => {
+    const [model, setModel] = useState("chatgpt")
+    const [isTextareaDisabled, setIsTextareaDisabled] = useState(false);
+    const [initialContent, setInitialContent] = useState("Kamu adalah seorang asisten yang memberikan jawaban singkat menggunakan bahasa indonesia");
+    const initialMessages: Message[] = [
+        {
+            id: Date.now().toString(),
+            role: 'system',
+            content: initialContent
+        }
+    ];
+    const [chatConfig, setChatConfig] = useState({ initialMessages });
+
+    
     const ref = useRef<HTMLDivElement>(null)
-    const { messages, input, handleInputChange, isLoading, handleSubmit: originalHandleSubmit, error } = useChat({
-        // initialMessages: [
-        //     {
-        //         id: Date.now().toString(),
-        //         role: 'system',
-        //         content: 'Kamu adalah seorang asisten yang memberikan jawaban singkat menggunakan bahasa indonesia'
-        //     }
-        // ]
-    });
+    const { messages, input, handleInputChange, isLoading, handleSubmit: originalHandleSubmit, error } = useChat(chatConfig);
 
     const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        const modelValue = "chatgpt";
+        const modelValue = model;
 
         const data:Record<string, string> = {
             model: modelValue,
         };
 
         originalHandleSubmit(event, {data});
+        setIsTextareaDisabled(true);
+    };
+
+    const handleModelChange = (value: string) => {
+        console.log(value)
+        setModel(value);
+    };
+
+    const handleInitialContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newContent = event.target.value;
+        setInitialContent(newContent);
+        setChatConfig({ initialMessages: [{ id: Date.now().toString(), role: 'system', content: newContent }] });
+        console.log(initialContent)
     };
 
     useEffect(() => {
@@ -41,73 +67,99 @@ export const Chat = () => {
     }, [messages])
   return (
     <div className="text-zinc-700">
-        <div className="container">
-            <div className="flex flex-col w-full max-w-md md:max-w-xl py-24 mx-auto stretch h-screen">
-                <div className="mb-4">
-                    <h1 className="text-center text-2xl">AI Chabot</h1>
-                    <p className="text-center text-xs">Created By <a href="https://github.com/tantowish" target="_blank">TOSHKA POLKA</a></p>
+        <div className="p-4 lg:p-8 lg:h-screen">
+            <div className="py-2 lg:px-4 lg:py-0 mb-4">
+                <h1 className="text-left text-2xl font-bold">AI Chabot</h1>
+                <p className="text-left text-xs">Created By <a href="https://github.com/tantowish" target="_blank">TANTOWS</a> powered by <a href="https://carigi.id" target="_blank">CARIGI INDONESIA</a></p>
+            </div>
+            <div className="flex flex-wrap mx-auto stretch h-full">
+                <div className="w-full lg:w-1/2 lg:p-4 lg:h-3/4">
+                    <div className="flex flex-wrap justify-between items-center mb-4">
+                        <p>Select model: </p>
+                        <Select 
+                        onValueChange={handleModelChange}
+                        value={model}>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="GPT-4" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="chatgpt">GPT-4</SelectItem>
+                                <SelectItem value="gemini">Gemini-pro</SelectItem>
+                                <SelectItem value="claude">Claude-3-5-sonnet</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className="lg:h-3/4">
+                        <div className="flex flex-wrap justify-between items-center">
+                            <p>Initial Message:</p>
+                            <p className="text-xs">*reload to change</p>
+                        </div>
+                        <Textarea 
+                            disabled={isTextareaDisabled}
+                            onChange={handleInitialContentChange} 
+                            value={initialContent}
+                            className="mb-4 h-full"
+                        />
+                    </div>
                 </div>
 
-                <ScrollArea className="mb-2 h-3/4 rounded-md border p-3 md:p-4"
-                ref={ref}>
-                    {error && (
-                        <div className="text-sm text-red-400">{error.message}</div>
-                    )}
-                    {messages.map(m => (
-                        <div key={m.id} className="mr-6 whitespace-pre-wrap md:mr-12">
-                        {m.role === 'user' && (
-                            <div className="mb-6 flex gap-2 md:gap-3">
-                                <Avatar className="h-8 w-8 md:h-10 md:w-10">
-                                    <AvatarImage src=""/>
-                                    <AvatarFallback>us</AvatarFallback>
-                                </Avatar>
-                                <div className="mt-1.5">
-                                    <p className="font-semibold text-sm md:text-base">You</p>
-                                    <div className="mt-1.5 text-sm text-zinc-500 text-justify">{m.content}</div>
-                                </div>
-                            </div>
+                <div className="w-full lg:w-1/2 lg:p-4 lg:h-3/4">
+                    <ScrollArea className="lg:h-full h-96 mb-2 rounded-md border p-3 md:p-4"
+                    ref={ref}>
+                        {error && (
+                            <div className="text-sm text-red-400">{error.message}</div>
                         )}
-                        {m.role === 'assistant' && (
-                            <div className="mb-6 flex gap-2 md:gap-3">
-                                <Avatar className="h-8 w-8 md:h-10 md:w-10">
-                                    <AvatarFallback className="bg-emerald-500 text-white">AI</AvatarFallback>
-                                </Avatar>
-                                <div className="mt-1.5 w-full">
-                                    <div className="flex justify-between">
-                                        <p className="font-semibold text-sm md:text-base">Bot</p>
-                                        <CopyToClipboard message={m} />
+                        {messages.map(m => (
+                            <div key={m.id} className="mr-6 whitespace-pre-wrap md:mr-12">
+                            {m.role === 'user' && (
+                                <div className="mb-6 flex gap-2 md:gap-3">
+                                    <Avatar className="h-8 w-8 md:h-10 md:w-10">
+                                        <AvatarImage src=""/>
+                                        <AvatarFallback>us</AvatarFallback>
+                                    </Avatar>
+                                    <div className="mt-1.5">
+                                        <p className="font-semibold text-sm md:text-base">You</p>
+                                        <div className="mt-1.5 text-sm text-zinc-500 text-justify">{m.content}</div>
                                     </div>
-                                    <Markdown className="mt-1.5 text-sm text-zinc-500 text-justify">{m.content}</Markdown>
                                 </div>
+                            )}
+                            {m.role === 'assistant' && (
+                                <div className="mb-6 flex gap-2 md:gap-3">
+                                    <Avatar className="h-8 w-8 md:h-10 md:w-10">
+                                        <AvatarFallback className="bg-emerald-500 text-white">AI</AvatarFallback>
+                                    </Avatar>
+                                    <div className="mt-1.5 w-full">
+                                        <div className="flex justify-between">
+                                            <p className="font-semibold text-sm md:text-base">Bot</p>
+                                            <CopyToClipboard message={m} />
+                                        </div>
+                                        <div className="mt-1.5 text-sm text-zinc-500 text-justify">{m.content}</div>
+                                    </div>
+                                </div>
+                            )}
                             </div>
-                        )}
-                        </div>
-                    ))}
-                </ScrollArea>
-                {/* Input */}
-                <form onSubmit={handleSubmit} className="relative">
-                    <Input
-                        className="hidden"
-                        value={"chatgpt"}
-                        name="model"
-                    />
-                    <Input
-                        className="pr-12 placeholder:italic placeholder:text-zinc-600/75 focus-visible:ring-zinc-500"
-                        value={input}
-                        placeholder="Ask me anything"
-                        onChange={handleInputChange}
-                    />
+                        ))}
+                    </ScrollArea>
+                    {/* Input */}
+                    <form onSubmit={handleSubmit} className="relative">
+                        <textarea
+                            className="w-full border border-1 rounded-md p-4 placeholder:italic placeholder:text-zinc-600/75 focus-visible:ring-zinc-500"
+                            value={input}
+                            placeholder="Send message..."
+                            onChange={handleInputChange}
+                        />
 
-                    <Button
-                        size='icon'
-                        type='submit'
-                        variant='secondary'
-                        disabled={isLoading}
-                        className='absolute right-1 top-1 h-8 w-10'
-                    >
-                        <SendHorizonalIcon className="h-4 w-4" />
-                    </Button>
-                </form>
+                        <Button
+                            size='icon'
+                            type='submit'
+                            variant='secondary'
+                            disabled={isLoading}
+                            className='absolute right-1 bottom-3 h-8 w-10'
+                        >
+                            <SendHorizonalIcon className="h-4 w-4" />
+                        </Button>
+                    </form>
+                </div>
             </div>  
         </div>
     </div>
